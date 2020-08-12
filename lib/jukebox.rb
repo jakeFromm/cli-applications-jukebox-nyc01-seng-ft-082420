c85ae0ec89dfad8185aa3929f23e109c5091891d
Jacob Fromm <jacobfromm@gmail.com>
2020-08-12 20:07:40 +0000

    Automatically backed up by Learn

--- lib/jukebox.rb
+++ lib/jukebox.rb
@@ -1,1 +1,61 @@
-# Add your code here
+require "pry"
+
+songs = [
+  "Phoenix - 1901",
+  "Tokyo Police Club - Wait Up",
+  "Sufjan Stevens - Too Much",
+  "The Naked and the Famous - Young Blood",
+  "(Far From) Home - Tiga",
+  "The Cults - Abducted",
+  "Phoenix - Consolation Prizes",
+  "Harry Chapin - Cats in the Cradle",
+  "Amos Lee - Keep It Loose, Keep It Tight"
+]
+
+def help
+  puts "I accept the following commands:"
+  puts "- help : displays this help message"
+  puts "- list : displays a list of songs you can play"
+  puts "- play : lets you choose a song to play"
+  puts "- exit : exits this program"
+end
+
+def list(songs)
+  songs.each_with_index do |song, index|
+    puts "#{index + 1}. #{song}"
+  end
+end
+
+def play(songs)
+  puts "Please enter a song name or number:"
+  input = gets.strip
+  song_num = input.to_i - 1
+  valid = false
+  songs.each_with_index do |song, index|
+    if song_num == index
+      puts "Playing #{songs[index]}"
+      valid = true
+    elsif input == song
+      puts "Playing #{song}"
+      valid = true
+    end
+  end
+  puts "Invalid input, please try again" if valid == false
+end
+
+def exit_jukebox
+  puts "Goodbye"
+end
+
+def run
+  loop do
+    puts "Please enter a command:"
+    input = gets.strip
+    binding.pry
+    case input
+    when "exit"
+      exit_jukebox
+      break
+    end
+  end
+end
